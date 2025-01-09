52d30475c4af813e8967e0bc89cac0696938c7cb
Robert Handi <roberthandiwijaya@gmail.com>
2025-01-10 00:58:25 +0700

    add login required in python CBV

--- app/views.py
+++ app/views.py
@@ -1,18 +1,25 @@
+from typing import Any
+
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from app.models import Article
 from django.views.generic import CreateView, ListView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 # @login_required
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
     template_name = "app/home.html"
     model = Article
     context_object_name = "articles"
 
+    def get_queryset(self) -> QuerySet[Any]:
+        return Article.objects.filter(creator=self.request.user).order_by("-created_at")
 
-class ArticleCreateView(CreateView):
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     fields = ["title", "status", "content", "twitter_post"]
     template_name = "app/article_create.html"
@@ -23,16 +30,22 @@ class ArticleCreateView(CreateView):
         return super().form_valid(form)
 
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Article
     fields = ["title", "status", "content", "twitter_post"]
     template_name = "app/article_update.html"
     context_object_name = "article"
     success_url = reverse_lazy("home")
 
+    def test_func(self) -> bool | None:
+        return self.request.user == self.get_object().creator
 
-class ArticleDeleteView(DeleteView):
+
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Article
     template_name = "app/article_delete.html"
     context_object_name = "article"
     success_url = reverse_lazy("home")
+
+    def test_func(self) -> bool | None:
+        return self.request.user == self.get_object().creator
